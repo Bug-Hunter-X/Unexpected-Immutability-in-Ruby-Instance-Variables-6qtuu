632cdb2f080f4e139e2b938790acb88325e195d0
BugHunter-X <askzenai@gmail.com>
2024-12-09 17:28:07 +0100

    Add bugSolution.rb file

---
+++ bugSolution.rb
@@ -0,0 +1,14 @@
+```ruby
+class MyClass
+  attr_accessor :value # This line adds both getter and setter methods
+
+  def initialize(value)
+    @value = value
+  end
+end
+
+my_object = MyClass.new(10)
+puts my_object.value # Output: 10
+my_object.value = 20 # Now this works!
+puts my_object.value # Output: 20
+```
